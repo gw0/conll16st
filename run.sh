ee7f0acbc63635348debcabe2d2942a59b095861
gw0 [http://gw.tnode.com/] <gw.2018@ena.one>
2018-03-19 14:48:33 +0100

    Add script to get results of simple parser.

--- run.sh
+++ run.sh
@@ -1,14 +1,42 @@
 # Run baseline discourse sense classifier with most common class
 
-mkdir ../ex/majority-en ../ex/majority-en/{trial,train,valid,test,blind}
-./sample_sup_parser.py en ../data/conll16st-en-03-29-16-trial ../ex/majority-en/trial/ ../ex/majority-en/trial/
-./sample_sup_parser.py en ../data/conll16st-en-03-29-16-train ../ex/majority-en/train/ ../ex/majority-en/train/
-./sample_sup_parser.py en ../data/conll16st-en-03-29-16-dev ../ex/majority-en/valid/ ../ex/majority-en/valid/
-./sample_sup_parser.py en ../data/conll16st-en-03-29-16-test ../ex/majority-en/test/ ../ex/majority-en/test/
-./sample_sup_parser.py en ../data/conll15st-en-03-29-16-blind-test ../ex/majority-en/blind/ ../ex/majority-en/blind/
+# conll16st - English datasets
+mkdir ../ex/majority-en ../ex/majority-en/{trial,valid,test,blind}
 
-mkdir ../ex/majority-zh ../ex/majority-zh/{train,valid,test,blind}
-./sample_sup_parser.py zh ../data/conll16st-zh-01-08-2016-train ../ex/majority-en/train/ ../ex/majority-en/train/
-./sample_sup_parser.py zh ../data/conll16st-zh-01-08-2016-dev ../ex/majority-en/valid/ ../ex/majority-en/valid/
-./sample_sup_parser.py zh ../data/conll16st-zh-01-08-2016-test ../ex/majority-en/test/ ../ex/majority-en/test/
-./sample_sup_parser.py zh ../data/conll16st-zh-04-27-2016-blind-test ../ex/majority-en/blind/ ../ex/majority-en/blind/
+DATA_DIR='../data/conll16st-en-03-29-16-trial'
+OUTPUT_DIR='../ex/majority-en/trial'
+./sample_sup_parser.py en "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR"
+./tira_sup_eval.py "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR" 2>&1 | tee "$OUTPUT_DIR/tira_sup_eval.log"
+
+DATA_DIR='../data/conll16st-en-03-29-16-dev'
+OUTPUT_DIR='../ex/majority-en/valid'
+./sample_sup_parser.py en "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR"
+./tira_sup_eval.py "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR" 2>&1 | tee "$OUTPUT_DIR/tira_sup_eval.log"
+
+DATA_DIR='../data/conll16st-en-03-29-16-test'
+OUTPUT_DIR='../ex/majority-en/test'
+./sample_sup_parser.py en "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR"
+./tira_sup_eval.py "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR" 2>&1 | tee "$OUTPUT_DIR/tira_sup_eval.log"
+
+DATA_DIR='../data/conll15st-en-03-29-16-blind-test'
+OUTPUT_DIR='../ex/majority-en/blind'
+./sample_sup_parser.py en "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR"
+./tira_sup_eval.py "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR" 2>&1 | tee "$OUTPUT_DIR/tira_sup_eval.log"
+
+# conll16st - Chinese datasets
+mkdir ../ex/majority-zh ../ex/majority-zh/{valid,test,blind}
+
+DATA_DIR='../data/conll16st-zh-01-08-2016-dev'
+OUTPUT_DIR='../ex/majority-zh/valid'
+./sample_sup_parser.py zh "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR"
+./tira_sup_eval.py "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR" 2>&1 | tee "$OUTPUT_DIR/tira_sup_eval.log"
+
+DATA_DIR='../data/conll16st-zh-01-08-2016-test'
+OUTPUT_DIR='../ex/majority-zh/test'
+./sample_sup_parser.py zh "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR"
+./tira_sup_eval.py "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR" 2>&1 | tee "$OUTPUT_DIR/tira_sup_eval.log"
+
+DATA_DIR='../data/conll16st-zh-04-27-2016-blind-test'
+OUTPUT_DIR='../ex/majority-zh/blind'
+./sample_sup_parser.py zh "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR"
+./tira_sup_eval.py "$DATA_DIR" "$OUTPUT_DIR" "$OUTPUT_DIR" 2>&1 | tee "$OUTPUT_DIR/tira_sup_eval.log"
